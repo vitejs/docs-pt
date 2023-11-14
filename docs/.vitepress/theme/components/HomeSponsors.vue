ddae582ceb060fb3b86e2c5d879b7dc111c24166
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-11-14 17:24:22 +0100

    chore: update the code of `components/homesponsors.vue`

--- docs/.vitepress/theme/components/HomeSponsors.vue
+++ docs/.vitepress/theme/components/HomeSponsors.vue
@@ -8,9 +8,7 @@ const { data } = useSponsor()
 <template>
   <VPHomeSponsors
     v-if="data"
-    message="Vite é gratuita e de código-aberto, tornada possível graças aos nossos maravilhosos patrocinadores."
-    action-text="Torna-te um patrocinador"
-    action-link="https://github.com/sponsors/yyx990803"
+    message="Vite é gratuita e de código-aberto, possível graças aos patrocinadores maravilhosos."
     :data="data"
   />
   <div class="action">
