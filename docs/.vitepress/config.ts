b3d9a726a907556b73b42f57e41c31203da8814e
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-04-09 07:30:32 +0100

    docs: add mastodon link to `resources` navigation
    closes #397

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -148,6 +148,10 @@ export default defineConfig({
           {
             items: [
               {
+                text: 'Mastodon',
+                link: 'https://elk.zone/m.webtoo.ls/@vite',
+              },
+              {
                 text: 'X (Twitter)',
                 link: 'https://x.com/vite_js'
               },
