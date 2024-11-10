cc84e1605df198c90c367aac86698643d8de2035
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-11-10 21:30:52 +0100

    fix(docs): adjust the visual presentation of the homepage

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,12 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './styles/vars.css'
-import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
+import AsideSponsors from './components/AsideSponsors.vue'
 import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import '@shikijs/vitepress-twoslash/style.css'
+import './styles/vars.css'
+import './styles/landing.css'
 import 'virtual:group-icons.css'
 
 export default {
