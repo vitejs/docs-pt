2ab49fc70a760fe751c3fafaca7b27c05170b5e8
Nazaré da Piedade <nazarepiedady@gmail.com>
2025-01-19 15:36:38 +0100

    docs: vite 6 announcement post
    closes #715

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,6 +7,7 @@ import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import '@shikijs/vitepress-twoslash/style.css'
 import './styles/vars.css'
 import './styles/landing.css'
+import YouTubeVideo from './components/YouTubeVideo.vue'
 import 'virtual:group-icons.css'
 
 export default {
@@ -18,6 +19,7 @@ export default {
   },
   enhanceApp({ app }) {
     app.component('SvgImage', SvgImage)
+    app.component('YouTubeVideo', YouTubeVideo)
     app.use(TwoslashFloatingVue)
   }
 } satisfies Theme
